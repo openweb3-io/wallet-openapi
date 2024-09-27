ea42c9bd6cbd2d9163cfc26f7cfe0d05fc78a795
Pharaoh <pharaoh@mtsocialdao.com>
2024-09-27 19:32:56 +0800

    fix: update webhook event

--- go/webhook_event.go
+++ go/webhook_event.go
@@ -1,21 +1,22 @@
 package wallet
 
-import "time"
+import (
+	"time"
+)
 
 type WebhookEvent struct {
-	Type    string      `json:"type"`
-	Payload Transaction `json:"payload"`
+	Type    string             `json:"type"`
+	Payload TransactionPayload `json:"payload"`
 }
 
 type TransactionPayload struct {
-	Id        string         `json:"id"`
-	Type      string         `json:"type"`
-	WalletId  string         `json:"walleId"`
-	AccountId string         `json:"accountId"`
-	Gateway   string         `json:"gateway"`
-	Amount    CurrencyAmount `json:"amount"`
-	Status    string         `json:"status"`
-	CreatedAt time.Time      `json:"createdAt"`
+	TransactionId string         `json:"transactionId"`
+	Type          string         `json:"type"`
+	WalletId      string         `json:"walletId"`
+	Gateway       string         `json:"gateway"`
+	Amount        CurrencyAmount `json:"amount"`
+	Status        string         `json:"status"`
+	CreatedAt     time.Time      `json:"createdAt"`
 }
 
 type CurrencyAmount struct {
